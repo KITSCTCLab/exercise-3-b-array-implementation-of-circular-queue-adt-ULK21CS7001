6de88e5ed7749dd8b6323ff8a2d7eeb5c4b83ce7
ULK21CS7001 <110515279+ULK21CS7001@users.noreply.github.com>
2022-08-12 19:02:32 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,50 +1,84 @@
-class CircularQueue:
+class MyCircularQueue:
+    def __init__(self, size: int):
+        self.size=size
+        self.queue=[None]*size
+        self.rear=-1
+        self.front=-1
 
-    #Constructor
-    def__init__(self):
-        self.queue = list()
-        self.head = 0
-        self.tail = 0
-        self.maxSize = 8
+    def enqueue(self, value: int) -> bool:
+       
+        if(self.is_full()==False):
+            if(self.front==-1):
+                self.front=0
+                self.rear=0
+                self.queue[self.rear]=value
+            else:
+                self.rear=(self.rear+1)%self.size
+                self.queue[self.rear]=value
+            return True
+        else:
+            return False
 
-    #Adding elements to the queue
-    def enqueue(self,data):
-        if self.size() == self.maxSize-1:
-            return ("Queue Full!")
-        self.queue.append(data)
-        self.tail = (self.tail + 1) % self.maxSize
-        return True
+    def dequeue(self) -> bool:
+        if(self.is_empty()==False):
+            if(self.front==self.rear):
+                self.front=-1
+                self.rear=-1
+                return True
+            else:
+                self.front=(self.front+1)%self.size
+                return True
+        else:
+            return False
+                
 
-    #Removing elements from the queue
-    def dequeue(self):
-        if self.size()==0:
-            return ("Queue Empty!") 
-        data = self.queue[self.head]
-        self.head = (self.head + 1) % self.maxSize
-        return data
+    def get_front(self) -> int:
+        if(self.is_empty()==False):
+            return self.queue[self.front]
+        else:
+            return -1
 
-    #Calculating the size of the queue
-    def size(self):
-        if self.tail>=self.head:
-            return (self.tail-self.head)
-        return (self.maxSize - (self.head-self.tail))
+    def get_rear(self):
+        if(self.is_empty()==False):
+            return self.queue[self.rear]
+        else:
+            return -1
 
-q = CircularQueue()
-print(q.enqueue(1))
-print(q.enqueue(2))
-print(q.enqueue(3))
-print(q.enqueue(4))
-print(q.enqueue(5))
-print(q.enqueue(6))
-print(q.enqueue(7))
-print(q.enqueue(8))
-print(q.enqueue(9))
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
+    def is_empty(self):
+        return self.front==-1
+
+    def is_full(self):
+        return (self.rear+1)%self.size==self.front
+           
+
+
+# Do not change the following code
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+data = []
+for item in input().split(','):
+    item = item.strip()
+    if item == '-':
+        data.append([])
+    else:
+        data.append([int(item)])
+obj = MyCircularQueue(data[0][0])
+result = []
+for i in range(len(operations)):
+    if i == 0:
+        result.append(None)
+    elif operations[i] == "enqueue":
+        result.append(obj.enqueue(data[i][0]))
+    elif operations[i] == "get_rear":
+        result.append(obj.get_rear())
+    elif operations[i] == "get_front":
+        result.append(obj.get_front())
+    elif operations[i] == "dequeue":
+        result.append(obj.dequeue())
+    elif operations[i] == "is_full":
+        result.append(obj.is_full())
+    elif operations[i] == "is_empty":
+        result.append(obj.is_empty())
+
+print(result)
